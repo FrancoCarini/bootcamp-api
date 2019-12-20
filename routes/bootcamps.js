d9a22dd080149d73c3f794f2120f395857aa640f
Franco Carini <carini.franco@gmail.com>
2019-12-20 16:03:16 -0300

    Added controllers dir and update bootcamps definition routes

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const {
+  getBootcamps, 
+  createBootcamp, 
+  getBootcamp,
+  updateBootcamp, 
+  deleteBootcamp
+} = require('../controllers/bootcamps')
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: 'Show all bootcamps'
-  })
-})
+
+router
+  .route('/')
+  .get(getBootcamps)
+  .post(createBootcamp)
+
+router.route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp)
 
 module.exports = router
