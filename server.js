82baae86b42adf2be3d53aab1d235c16c4ec3118
Franco Carini <carini.franco@gmail.com>
2019-12-23 10:43:02 -0300

    Added db.js for conn to MongoDb. Added Models Dir with Bootcamp model

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const connectDB = require('./config/db')
+
+//Load env vars
+dotenv.config({path: './config/config.env'})
+
+//Connection to DB
+connectDB()
 
 //Route Files
 const bootcamps = require('./routes/bootcamps')
-
-dotenv.config({path: './config/config.env'})
 
 const app = express()
 
@@ -19,7 +24,13 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(process.env.PORT || 5000, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
   console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 })
 
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`)
+  //Close server and exit process
+  server.close(() => process.exit(1))
+})
