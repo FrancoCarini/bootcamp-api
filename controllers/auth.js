e9be8d3c598e46d4f7371ab53e72039c7cb7f74f
Franco Carini <carini.franco@gmail.com>
2020-03-04 11:11:10 -0300

    Added all functionality for Forget Password action. Added config example file

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
+const crypto = require('crypto')
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middlewares/async')
+const sendEmail = require('../utils/sendEmail')
 const User = require('../models/User')
 
 // @desc      Register User
@@ -77,4 +79,72 @@ exports.getMe = asyncHandler(async(req, res, next) => {
     success: true,
     data: req.user
   })
+})
+
+
+
+// @desc      Forgot password
+// @route     POST /api/v1/auth/forgotpassword
+// @access    Public
+exports.forgotPassword = asyncHandler(async(req, res, next) => {
+  const user = await User.findOne({email: req.body.email})
+
+  if (!user) {
+    next(new ErrorResponse('Theres no user with that email'), 404)  
+  }
+
+  //Get Reset Token
+  const resetToken = user.getResetPasswordToken()
+  
+  //Save password
+  await user.save({validateBeforeSave: false})
+
+  //Create reset url 
+  const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or seomeone else) has requested the reset of 
+  a password. Please make a PUT request to: \n\n ${resetUrl}`
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message
+    })
+    res.status(200).json({success: true, data: 'Email sent'})
+  } catch (err) {
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpired = undefined
+    await user.save({validateBeforeSave: false})
+    return next(new ErrorResponse('Email could not be sent'), 500)  
+  }
+})
+
+// @desc      Reset password
+// @route     PUT /api/v1/auth/resetpassword/:resettoken
+// @access    Public
+exports.resetPassword = asyncHandler(async(req, res, next) => {
+  //Get hashed token 
+  const resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(req.params.resettoken)
+    .digest('hex')
+
+  //Find user by reset token and while resetPasswordExpired be > than now
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: {$gt: Date.now()}
+  })
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid Token'), 400)  
+  }
+
+  //Set the new password
+  user.password = req.body.password
+  user.resetPasswordToken = undefined
+  user.resetPasswordExpired = undefined
+  await user.save()
+
+  sendTokenResponse(user, 200, res)
 })
