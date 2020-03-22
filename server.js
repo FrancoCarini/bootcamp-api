169df2f72653f4e8f970e1648928878109e5ff75
Franco Carini <carini.franco@gmail.com>
2020-03-22 04:22:22 -0300

    Added CORS, HPP, Rate Limit, XSS

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const cookieParser = require('cookie-parser')
 const mongoSanitize = require('express-mongo-sanitize')
 const helmet = require('helmet')
 const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
 const errorHandler = require('./middlewares/errors')
 const connectDB = require('./config/db')
 
@@ -47,6 +50,19 @@ app.use(helmet())
 
 // Prevent XSS attacks 
 app.use(xss())
+
+//Rate limiting
+const limiter = rateLimit({
+  windowMs: 60000, //1 minutes
+  max: 2
+})
+app.use(limiter)
+
+// Prevent http param pollution
+app.use(hpp())
+
+// Enable CORS
+app.use(cors())
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
